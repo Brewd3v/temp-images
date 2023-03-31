8643e221291160b6d598899591b5328dadf7a88f
James Marx <jmarx@haproxy.com>
2023-03-31 13:49:03 +0100

    working

---
+++ netlify/functions/newsletter-image/newsletter-image.js
@@ -0,0 +1,21 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  try {
+    const url = "https://stately-axolotl-d70a35.netlify.app/image.png"
+    const response = await fetch(url)
+    const buffer = await response.arrayBuffer()
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "image/png",
+      },
+      body: Buffer.from(buffer).toString("base64"),
+      isBase64Encoded: true,
+    };
+    
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
